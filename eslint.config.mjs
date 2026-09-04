bd3b40d053d99b346ed79b2f581141b40fbbc564
Sindhu <sindhuchitukula521@gmail.com>
2026-09-04 16:18:37 +0530

    Configure frontend and backend projects

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,7 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
@@ -12,7 +13,11 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+
+    // Backend is a separate TypeScript project:
+    "server/**",
   ]),
 ]);
 
 export default eslintConfig;
+
